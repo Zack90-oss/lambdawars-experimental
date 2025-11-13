fdad01981f4bced1c070de9c79a72f640abf27cf
Zack90-oss <sy4yow.egor2017@yandex.ru>
2025-11-14 00:31:56 +0500

    Setting MAX_EDICT_BITS to 12 (1 more) and printing experemental message

--- src/game/server/lambdawars/wars_weapon.cpp
+++ src/game/server/lambdawars/wars_weapon.cpp
@@ -158,6 +158,8 @@ int CWarsWeapon::ShouldTransmit( const CCheckTransmitInfo *pInfo )
 		return FL_EDICT_DONTSEND;
 	}
 #endif // 0
+	Msg("message from CWarsWeapon::ShouldTransmit");
+	Msg(" MAX_EDICT_BITS = %d\n", MAX_EDICT_BITS);
 
 	CBaseEntity *pRecipientEntity = CBaseEntity::Instance( pInfo->m_pClientEnt );
 	Assert( pRecipientEntity->IsPlayer() );
